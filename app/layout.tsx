9f090595aad461788dc49c4c4b30245c7eb2817e
rochardEloi <rochardeliamar.eloi@uniq.edu>
2024-11-21 16:44:15 -0500

    New commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export default function RootLayout({
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
-          suppressHydrationWarning
         >
           {children}
           <Toaster />
